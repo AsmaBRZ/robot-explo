5c83c1f5506e5d5fcd8877b5522c1f7e86904e98
Asma <asmabrazi27@gmail.com>
2019-06-17 11:17:19 +0200

    detection of corners based on good features

--- app/explo/python/utils.py
+++ app/explo/python/utils.py
@@ -226,6 +226,28 @@ def goodFeatureCorner(im):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     #return corners
+def StreamGFCornerDetection():
+    camera=PiCamera()
+    camera.resolution=(600,400)
+    camera.framerate=32
+    rawCapture=PiRGBArray(camera,size=(600,400))
+    time.sleep(0.1)
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
+        goodFeatureCorner(image)
+        # show the frame
+        cv2.imshow("Frame", image)
+
+        key = cv2.waitKey(1) & 0xFF
+ 
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+ 
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 def PictureGFCornerDetection():
     goodFeatureCorner(takePicture())
 
