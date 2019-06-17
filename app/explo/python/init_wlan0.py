4a031a57ad75d78a68532fdfab36f7de7d3ff25a
Asma <asmabrazi27@gmail.com>
2019-06-17 12:44:15 +0200

    update the IP of Rasp

---
+++ app/explo/python/init_wlan0.py
@@ -0,0 +1,12 @@
+'''
+    File name: init_wlan0.py
+    Author: Asma BRAZI
+    Date created: 17/05/2019
+    Python Version: 3.6.7
+'''
+import os
+os.system("sudo su")
+os.system("wpa_supplicant -i wlan0 ")
+os.system("wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf")
+os.system("dhclient wlan0")
+os.system("exit")
