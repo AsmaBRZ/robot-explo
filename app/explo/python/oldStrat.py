2d20ebf5ce6b9c6f38ae784e17ca18f9441c4d90
Asma <asmabrazi27@gmail.com>
2019-06-20 10:07:33 +0200

    Second strategy - lines merge

--- app/explo/python/oldStrat.py
+++ app/explo/python/oldStrat.py
@@ -204,8 +204,138 @@ def intersection(l1,l2):
         return result.coords[0]
     if isinstance(result, Point):
         return result.x,result.y
+def newCorner(r,set,borders):
+    for b in borders:
+        if abs(r[0]-b)<5 or abs(r[1]-b)<5 or r[1]<borders[2]/3:
+            return False 
+    for t in set:
+        if abs(t[0]-r[0]<10) and abs(t[1]-r[1]<10) and r[1]>borders[2]/3:
+            return False
+    return True
 
+######################################
+# First method for corner detection  #
+######################################
+def goodFeatureCorner(im):
+    img = cv2.imread(im)
+    # convert image to gray scale image 
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+    # detect corners with the goodFeaturesToTrack function. 
+    corners = cv2.goodFeaturesToTrack(gray, 10, 0.01, 500) 
+    corners = np.int0(corners) 
+  
+    # we iterate through each corner,  
+    # making a circle at each point that we think is a corner. 
+    for i in corners: 
+        x, y = i.ravel() 
+        print(x,y)
+        cv2.circle(img, (x, y), 3, 255, -1) 
+  
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #return corners
+def StreamGFCornerDetection():
+    camera=PiCamera()
+    camera.resolution=(600,400)
+    camera.framerate=32
+    rawCapture=PiRGBArray(camera,size=(600,400))
+    time.sleep(0.1)
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
+        goodFeatureCorner(image)
+        # show the frame
+        cv2.imshow("Frame", image)
+
+        key = cv2.waitKey(1) & 0xFF
+ 
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+ 
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+def PictureGFCornerDetection():
+    goodFeatureCorner(takePicture())
+
+######################################
+# Second method for corner detection #
+######################################
+def HoughLinesCorner(im):
+    dic={}
+    img =  cv2.imread(im)
+    n,m,_=np.array(img).shape
+    borders=[0.0,n,m]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 75, 150)
+    minLG=30
+    maxLG = 100
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=60,maxLineGap=maxLG)
+    #sort lines by length
+    sortedLineLength=[]
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        dic[-np.sqrt((x1-x2)**2+(y1-y2)**2)]=line[0]
+        sortedLineLength.append(np.array([line[0],-np.sqrt((x1-x2)**2+(y1-y2)**2)]))
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    dic= collections.OrderedDict(sorted(dic.items()))
+    sortedDic={}
+    for k, v in dic.items():
+        sortedDic[-k]=v
+    cornersFiltred=[]
+    intersections=[]
+
+    #calculate the intersections
+    for k1,l1 in sortedDic.items():
+        for k2,l2 in sortedDic.items():
+            r=intersection(l1,l2)
+            if r:
+                bo=True
+                x,y=r
+                if len(cornersFiltred)==0:
+                    for b in borders:
+                        if abs(r[0]-b)<5 or abs(r[1]-b)<5:
+                            bo=False
+                    if bo:
+                        cornersFiltred.append(r)
+                        cv2.circle(img, (int(x), int(y)), 3, (0, 0, 255), -1)
+                else:
+                    if newCorner(r,cornersFiltred,borders):
+                        cornersFiltred.append(r)
+                        cv2.circle(img, (int(x), int(y)), 3, (0, 0, 255), -1)
+    #cv2.imshow("Edges", edges)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #return cornersFiltred
+
+def StreamHLCornerDetection():
+    camera=PiCamera()
+    camera.resolution=(600,400)
+    camera.framerate=32
+    rawCapture=PiRGBArray(camera,size=(600,400))
+    time.sleep(0.1)
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
+        HoughLinesCorner(image)
+        # show the frame
+        cv2.imshow("Frame", image)
+
+        key = cv2.waitKey(1) & 0xFF
+ 
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+ 
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+def PictureHLCornerDetection():
+    HoughLinesCorner(takePicture())
 
 #Calculate the equation of the segment with two points p1 and p2
 #return the coef a and  of the equation
@@ -242,7 +372,7 @@ def distLineLine(l1,l2):
     return max(d1,d2,d3,d4)
 
 #Calculate the longest distance between two lines, by checking each extremity with the other line. 
-def minDistLineLine(l1,l2,thresholdA=0.01,thresholdC=0.25):
+def minDistLineLine(l1,l2,thresholdA=0.1,thresholdC=1):
     x1,y1,x2,y2=l1
     x3,y3,x4,y4=l2
     a1,b1,c1=coefLine([x1,y1],[x2,y2])
@@ -253,7 +383,7 @@ def minDistLineLine(l1,l2,thresholdA=0.01,thresholdC=0.25):
 #Treat by a couple of lines from the entry 'lines', if they are too close depending on a threshold, they are merged
 #Teh set of kept lines is returned
 def mergeLines(lines,threshold=30):
-    filtred_lines={}
+    filtred_lines=[]
     lines_copy=lines.copy()
     dic={}
     #sort lines by length
@@ -271,15 +401,13 @@ def mergeLines(lines,threshold=30):
     dicLines = { i : sortedDic[i] for i in range(0, len(sortedDic) ) }
     for Kl1 in list(dicLines):
         if Kl1 in dicLines.keys():
-            Vl1=dicLines[Kl1][1]
+            Vl1=dicLines[Kl1]
             Kl_OK=False
             #print('Kl1,Vl1',Kl1,Vl1)
-            cp=1
             for Kl2 in list(dicLines):
-                cp+=1
                 if Kl2  in dicLines.keys():
                     #print("Kl2",Kl2)
-                    Vl2=dicLines[Kl2][1]
+                    Vl2=dicLines[Kl2]
                     if Kl1!=Kl2:
                         #print('Kl2,Vl2',Kl2,Vl2)
                         #print('distance',dist)
@@ -290,26 +418,20 @@ def mergeLines(lines,threshold=30):
                             x0,y0,x1,y1=Vl2
                             l2_length=np.sqrt((x0-x1)**2+(y0-y1)**2)
                             if l1_length>l2_length:
-                                filtred_lines[Kl1]=Vl1
+                                filtred_lines.append(Vl1)
                             else:
-                                filtred_lines[Kl2]=Vl2
+                                filtred_lines.append(Vl1)
                             dicLines.pop(Kl1)
                             dicLines.pop(Kl2)
                             #print('Dic filt',dicLines.keys())
                             Kl_OK=True
                 if Kl_OK:
                     break
-            #L1 is far from all the other lines, we add it to the set of filtred lines and we delete it from the dictionary,
-            #so that it will not checked again with the other elements
-            if cp==len(dicLines) and (Kl1 not in filtred_lines):
-                filtred_lines[Kl1]=Vl1
-                dicLines.pop(Kl1)
     return filtred_lines
 
 #Use of the LSD detection in order to capture the necessary segments after filtering
 def LSDDetection(im):
     img = cv2.imread(im)
-    img_iter=img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray= cv2.Canny(gray, 100, 150)
     lsd = cv2.createLineSegmentDetector(0)
@@ -323,26 +445,23 @@ def LSDDetection(im):
         y1 = int(round(line[0][3]))
         if np.sqrt((x0-x1)**2+(y0-y1)**2)>20:
             if abs(x0-x1)<20:
-                cv2.line(img, (x0, y0), (x1,y1), (0,255,0), 1, cv2.LINE_AA)
+                #cv2.line(img, (x0, y0), (x1,y1), (0,255,0), 1, cv2.LINE_AA)
                 lines_vert.append(line[0])
             else:
-                cv2.line(img, (x0, y0), (x1,y1), (255,0,0), 1, cv2.LINE_AA)
                 lines_non_vert.append(line[0])
-
     #print("Vertical lines are:",lines_vert)
     #print("Non-Vertical lines are:",lines_non_vert)
    # print("lines_non_vert:",lines_non_vert)
     filtred_non_vert_lines=mergeLines(lines_non_vert)
     #print()
     #print('filtred_non_vert_lines: ',filtred_non_vert_lines)
-    for l1 in filtred_non_vert_lines.values():
-        cv2.line(img_iter, (l1[0], l1[1]), (l1[2],l1[3]), (255,0,0), 1, cv2.LINE_AA)
+    for l1 in filtred_non_vert_lines:
+        cv2.line(img, (l1[0], l1[1]), (l1[2],l1[3]), (255,0,0), 1, cv2.LINE_AA)
     filtred_vert_lines=mergeLines(lines_vert)
-    for l1 in filtred_vert_lines.values():
-        cv2.line(img_iter, (l1[0], l1[1]), (l1[2],l1[3]), (0,255,0), 1, cv2.LINE_AA)
+    for l1 in filtred_vert_lines:
+        cv2.line(img, (l1[0], l1[1]), (l1[2],l1[3]), (0,255,0), 1, cv2.LINE_AA)
     #drawn_img = lsd.drawSegments(img,lines)
     cv2.imshow("Image", img)
-    cv2.imshow("Image_ITER", img_iter)
     #cv2.imshow("Edges", gray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
