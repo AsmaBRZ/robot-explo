480e1faff1c220a75c942439d285b238c3063ab8
Asma <asmabrazi27@gmail.com>
2019-07-18 13:02:37 +0200

    Finished

--- app/explo/src/envStructures/Wall.java
+++ app/explo/src/envStructures/Wall.java
@@ -1,6 +1,5 @@
 package envStructures;
-import java.util.ArrayList;
-import java.util.List;
+
 /**
  * 
  * @author Clara Rigaud
@@ -8,63 +7,53 @@ import java.util.List;
  *
  */
 public class Wall extends RoomObject{
-	private List<RoomObject> objectsOnWall; // liste des objets places contre le mur de gauche a� droite
 	public boolean obstacle = false;
 	public double robotRotation;
 	public Vec2 robotPosition;
+
 	/**
 	 * 
-	 * @param id (int) Identifier of the wall
-	 * @param left (Point) left corner of the wall
-	 * @param right (Point) right corner of the wall
+	 * @param id unique identity of the wall
+	 * @param left coordinates of the left corner of the wall
+	 * @param right coordinates of the right corner of the wall
+	 * @param height the height of the wall in 0.01*cm
+	 * @param width the width of the wall in 0.01*cm
+	 * @param robotRotation the rotation of the robot in radian when the wall has been captured
+	 * @param robotPosition the position's coordinates of the robot  when the wall has been captured
 	 */
 	public Wall(int id, Point left, Point right, float  height,float width,double robotRotation,Vec2 robotPosition){
 		super(id, left, right, height, width);
-		objectsOnWall=new ArrayList<RoomObject>();
 		this.obstacle=false;
 		this.robotRotation=robotRotation;
 		this.robotPosition=robotPosition;
-		
+
 	}
+	/**
+	 * 
+	 * @param id unique identity of the wall
+	 * @param left coordinates of the left corner of the wall
+	 * @param right coordinates of the right corner of the wall
+	 * @param height the height of the wall in 0.01*cm
+	 * @param width the width of the wall in 0.01*cm
+	 * @param robotRotation the rotation of the robot in radian when the wall has been captured
+	 * @param robotPosition the position's coordinates of the robot  when the wall has been captured
+	 * @param obstacle if the robot has detect the wall with its sensors, the wall is considered as an obstacle, may be for a future verification 
+	 */
 	public Wall(int id, Point left, Point right, float  height,float width,double robotRotation, Vec2 robotPosition,boolean obstacle){
 		super(id, left, right, height, width);
-		objectsOnWall=new ArrayList<RoomObject>();
 		this.obstacle=obstacle;
 		this.robotRotation=robotRotation;
 		this.robotPosition=robotPosition;
-		
-	}
-	
-	public void setY(double y) {
-		float x=(float) this.cornerRight.getPosition().getX();
-		this.cornerLeft.setPosition(x, (float) (y));
-		this.cornerRight.setPosition(x, (float) (y));
-	}
-	public void cumulateWidth(float w) {
-		 this.setWidth(this.getWidth()+w);
-	}
-	public void cumulateCornerRight(Point c) {
-		this.cornerRight.setPosition(this.cornerRight.getPosition().add(c.getPosition()));
-	}
-
-	public void updateObjectPoints(){
-		// constrain objects to stay on the wall
-		for(int i = 0;i< objectsOnWall.size(); i++){
-			Vec2 oldCl = objectsOnWall.get(i).cornerLeft.position();
-			Vec2 oldCr = objectsOnWall.get(i).cornerRight.position();
-			Vec2 newoCl = Vec2.projectPointOnLine(oldCl, this.cornerRight.position(), this.cornerLeft.position());
-			Vec2 newoCr = Vec2.projectPointOnLine(oldCr, this.cornerRight.position(), this.cornerLeft.position());
-			objectsOnWall.get(i).cornerLeft.setPosition(newoCl);
-			objectsOnWall.get(i).cornerRight.setPosition(newoCr);
-		}
 	}
 	/**
-	 *  Unused 4 now - stores a RoomObject attached on the wall on the list
-	 * @param o Room object to add
+	 * 
+	 * @param y the coordinate of the corner of the Y-axis
 	 */
-	public void addObject(RoomObject o){
-		System.out.println("room in addobject"+o.toString());
-		this.objectsOnWall.add(o);
+	public void setY(double y) {
+		float x=(float) this.cornerRight.getPosition().getX();
+		//we assume having the same y for the two corners
+		this.cornerLeft.setPosition(x, (float) (y));
+		this.cornerRight.setPosition(x, (float) (y));
 	}
 	@Override
 	public String toString() {
@@ -72,7 +61,5 @@ public class Wall extends RoomObject{
 				+ ", robotPosition=" + robotPosition + ", cornerLeft=" + cornerLeft + ", cornerRight="
 				+ cornerRight + ", height=" + height + ", width=" + width + "]";
 	}
-
-
 
 }
