9669534fc65509ec55d2fa32455c68cded99ec10
Asma <asmabrazi27@gmail.com>
2019-07-18 15:36:27 +0200

    Finished

--- app/explo/src/explorator/Exploration.java
+++ app/explo/src/explorator/Exploration.java
@@ -39,16 +39,13 @@ public class Exploration {
 	private ArrayList<HashMap<String, String>> dbObjects=new ArrayList<HashMap<String, String>>();
 	private int cpWall=0; //counter of the detected walls
 	private boolean targetFound=false; //indicate whether the target has been found or not
-	//We set the distance with which the robot rolls 70 cm
+	//We set the distance with which the robot rolls 70 centimeter
 	private double distanceRob=70; //this distance may be changed. It is the current distance which the distance travel at each movement
-	private	Point cornerLeft, cornerRight; //the left anf right corners of the current detected wall
 	private int threshClose =150; //threshold indicating if the horizontal segment detected is too close or too far rom the robot
-	private double lastDistanceTraveled=0;  
-	float lenSeg,x0,y0,x1,y1,x0_tmp,y0_tmp,x1_tmp,y1_tmp;
-	double robRotation;
-	Vec2 robPosition;
+	private double lastDistanceTraveled=0; //the last distance traveled by the robot
 	List<ArrayList<Float>> data;
 	private boolean obstacle=false;
+
 	public Exploration() throws InterruptedException{
 		File configFile = new File("config.properties");
 		try {
@@ -77,10 +74,10 @@ public class Exploration {
 			if(!windows){
 				this.localURL="/"+this.localURL;
 			}
-			//read the database of objects, in our case we have three images: platon, pepper and the QR code
+			//Read the database of objects, in our case we have ponly one object: QR code
 			readDBObjects();
 			System.out.println("DB created");
-			//Init of the JME application, where we will display the environment's construction
+			//Initialization of the JME application, where we will display the environment's construction
 			env =  new InternalRepresentation(robot,explorationMode);
 			jmeApp = new Scene(env, robot);
 			this.jmeApp.start();
@@ -92,19 +89,20 @@ public class Exploration {
 	}
 
 	/**
-	 * Starts the exploration
+	 * Start the exploration
 	 */
 	public int start(){
 		//We ask the user the name of the object that robot have to search
 		Scanner sc = new Scanner(System.in);
 		int goal=-1;
+
 		System.out.println("Please enter the number corresponding to the target from the following list:");
 		System.out.println("0: QR code");
 		//System.out.println("1: Pepper");
 		//System.out.println("2: Platon");
-
 		String str = sc.nextLine();
-		goal=Integer.parseInt(str);
+
+		goal=Integer.parseInt(str); //goal is the name of the object we are searching for, currently it may be in[0.png, 1.png or 2.png]
 		switch(goal) {
 		case 0: System.out.println("Target: QR code"); break;
 		//case 1: System.out.println("Target: Pepper"); break;
@@ -112,12 +110,14 @@ public class Exploration {
 		default: System.out.println("Wrong number"); 
 		return 0;
 		}
+
+		//the robot explores until finding the target or finishing exploring the environment without finding the target
 		System.out.println("Exploration begins");
 		while(!this.targetFound){
 			int state;
 			state=explore(goal);
 			if(state==2) {
-				System.out.println("The robot has finished exploring its environment without finding the object!");
+				System.out.println("The robot has finished exploring its environment without finding the target !");
 				break;
 			}
 		}
@@ -125,58 +125,60 @@ public class Exploration {
 		try {
 			robot.disconnect();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return 1;
 	}
 
 	@SuppressWarnings("unused")
-	//target is the name of the object we re searching for, currently it may be in[0.png, 1.png or 2.png]
+	/**
+	 * 
+	 * @param target The target to find
+	 * @return 1 if the target has been found and 0 to continue exploring 
+	 */
 	private int explore(int target){
 		if(this.init) {
+			//After 1 iteration, the robot has initialized the exploration
 			this.init=false;
 		}
 		else {
+			//If the current position of the robot is equal to its initial one and that the robot is not at the initialization
+			//Than the environment has been totally explored without finding the target
 			if(this.robot.getPosition().equals(this.robot.getInitPosition())) {
 				return 2;
 			}
 		}
-		System.out.println("My walls are: "+this.env.getWalls().toString()+"\n0");
-
+		float lenSeg,x0,y0,x1,y1,x0_tmp,y0_tmp,x1_tmp,y1_tmp;
+		Point cornerLeft, cornerRight; //the left and right corners of the current detected wall
+		double robRotation; //the current rotation of the robot
+		Vec2 robPosition; //the current position of the robot
 		robRotation=this.robot.getRotation()-Math.PI/2;
 		robPosition=this.robot.getPosition();
 
-		System.out.println();
 		//Step 0: Capture the visual information			
-		// capture visual information
-		if(!obstacle) {
-			data=robot.captureData();
-			this.obstacle=false;
-		}
+		data=robot.captureData();
 
-		//System.out.println("Visual information: "+data.toString());
-
-		//retreive information about the object to find
+		//Step 1: Retrieve information about the object to find
+		//The first element of 'data' is the resolution, for the moment we do not use it
+		//The second element of 'data' contains the dimensions of the target. -1 everywhere if the robot did not detect anything
 		ArrayList<Float> dimensionsObj=data.get(1);
-		//System.out.println("dimensionsObj"+dimensionsObj);
 		float distanceToWall=dimensionsObj.get(0);
 		float startX=dimensionsObj.get(1);
 		float startY =dimensionsObj.get(2);
 		float endX=dimensionsObj.get(3);
 		float endY=dimensionsObj.get(4);
 
-		//retreive information about the dimensions of the walls
+		//The third element of 'data' contains information about the walls dimensions
 		float height=data.get(2).get(0)/100;
-		if(height==0) {
+		if(height==-1) {
 			height=1;
 		}
 		this.env.addHeight(height);
-		ArrayList<Float> width=data.get(3);
-		ArrayList<Float>  depthL=data.get(4);
-		ArrayList<Float>  depthR=data.get(5);
+		ArrayList<Float> width=data.get(3); //The fourth element of 'data' contains the dimensions of the wall in front of the robot
+		ArrayList<Float>  depthL=data.get(4); //The fifth element of 'data' contains the dimensions of the wall on the left of the robot
+		ArrayList<Float>  depthR=data.get(5); //The sixth element of 'data' contains the dimensions of the wall on the right of the robot
 
-		//retreive information about the distances captures by the autonomous robot's sensors
+		//The seventh element of 'data' contains the distances captured by the autonomous robot's sensors
 		ArrayList<Float>  disSensors=data.get(6);
 
 		if(startX!=-1) {
@@ -185,8 +187,11 @@ public class Exploration {
 			System.out.println("Target detected");
 			return 1;
 		}
+
+		//Step 2: building the detected wall
+
+		//If the robot's front sensors are activated, than an obstacle (eventual wall) has been detected 
 		if(disSensors.get(2)!=0|| disSensors.get(3)!=0 || disSensors.get(1)!=0){
-			//the autonomous robot is too close from the wall in front
 			height=1;
 			System.out.println("Obstalce in front of the autonomous robot");
 			try {
@@ -195,11 +200,14 @@ public class Exploration {
 				float xA=(float) 0.1;
 				float xB=(float) -0.1;
 
+				//transformation : translation and rotation of the robot to from the robot's system to the main system
 				x0=(float) (xA*((float)Math.cos(robRotation))-yA*Math.sin(robRotation)+robPosition.x);
 				y0=(float) (yA*((float)Math.cos(robRotation))+xA*Math.sin(robRotation)+robPosition.y);
 				x1=(float) (xB*((float)Math.cos(robRotation))-yB*Math.sin(robRotation)+robPosition.x);
 				y1=(float) (yB*((float)Math.cos(robRotation))+xB*Math.sin(robRotation)+robPosition.y);
 
+
+				//adjust the y coordinate to the mean of the y coordinate of the first corner and the second one
 				cornerLeft=new Point(x0,(y1+y0)/2);
 				cornerRight=new Point(x1,(y0+y1)/2);
 				lenSeg=(float) Math.sqrt(Math.pow((x0-x1),2)+Math.pow((y0-y1),2));
@@ -216,11 +224,11 @@ public class Exploration {
 
 				this.lastDistanceTraveled=0;
 				this.lastDistanceTraveled=this.robot.move(this.distanceRob);
-				double tmpDistanceTravelled=this.lastDistanceTraveled;
+				double tmpDistancetraveled=this.lastDistanceTraveled;
 				this.robot.rotate(-90);
 				this.robot.move(distanceRob);
 				this.robot.rotate(-90);
-				this.lastDistanceTraveled=tmpDistanceTravelled;
+				this.lastDistanceTraveled=tmpDistancetraveled;
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (InterruptedException e) {
@@ -228,24 +236,22 @@ public class Exploration {
 			}
 			return 0;
 		}
+
+
 		if(disSensors.get(0)!=0 ){
-			System.out.println("Obstalce on the left of the autonomous robot");
+			System.out.println("Obstacle on the left of the autonomous robot");
 			height=1;
 			double yA=-0.1;
 			double yB=0.1;
 			float xA=(float) -0.1;
 			float xB=(float) -0.1;
-			//rotation + translation according to rob's rotation + position
 
-			//System.out.println("ROb rotation is"+this.robRotation);
+			//transformation : translation and rotation of the robot to from the robot's system to the main system
 			x0=(float) (xA*((float)Math.cos(robRotation))-yA*Math.sin(robRotation)+robPosition.x);
 			y0=(float) (yA*((float)Math.cos(robRotation))+xA*Math.sin(robRotation)+robPosition.y);
 			x1=(float) (xB*((float)Math.cos(robRotation))-yB*Math.sin(robRotation)+robPosition.x);
 			y1=(float) (yB*((float)Math.cos(robRotation))+xB*Math.sin(robRotation)+robPosition.y);
-			x0/=100;
-			y0/=100;
-			x1/=100;
-			y1/=100;
+
 			cornerLeft=new Point(x0,y0);
 			cornerRight=new Point(x1,y1);
 			lenSeg=(float) Math.sqrt(Math.pow((x0-x1),2)+Math.pow((y0-y1),2));
@@ -267,15 +273,13 @@ public class Exploration {
 			double yB=0.1;
 			float xA=(float) 0.1;
 			float xB=(float) 0.1;
+
+			//transformation : translation and rotation of the robot to from the robot's system to the main system
 			x0=(float) (xA*((float)Math.cos(robRotation))-yA*Math.sin(robRotation)+robPosition.x);
 			y0=(float) (yA*((float)Math.cos(robRotation))+xA*Math.sin(robRotation)+robPosition.y);
 			x1=(float) (xB*((float)Math.cos(robRotation))-yB*Math.sin(robRotation)+robPosition.x);
 			y1=(float) (yB*((float)Math.cos(robRotation))+xB*Math.sin(robRotation)+robPosition.y);
 
-			x0/=100;
-			y0/=100;
-			x1/=100;
-			y1/=100;
 			cornerLeft=new Point(x0,y0);
 			cornerRight=new Point(x1,y1);
 			lenSeg=(float) Math.sqrt(Math.pow((x0-x1),2)+Math.pow((y0-y1),2));
@@ -293,9 +297,9 @@ public class Exploration {
 		if(disSensors.get(5)!=0 || disSensors.get(6)!=0 ){
 			System.out.println("Obstalce behind the autonomous robot");
 			try {
+				//move to face the wall behind the robot
 				this.lastDistanceTraveled=0;
 				this.lastDistanceTraveled=this.robot.move(distanceRob);
-				//Thread.sleep(this.timeForMove);
 				this.robot.rotate(90);
 				this.robot.rotate(90);
 			} catch (IOException | InterruptedException e) {
@@ -306,21 +310,24 @@ public class Exploration {
 		}
 
 
-		//building detected walls
+		//an horizontal wall has been detected
 		if(width.get(0)!=-1 && width.get(2)>threshClose){
 			y0=width.get(2);
 			y1=width.get(4);
 			x0=width.get(1);
 			x1=width.get(3);
 
+			//Convert the coordinates in pixel to centimeter
 			y0_tmp=pixToCmY(width.get(2));
 			y1_tmp=pixToCmY(width.get(4));
-
 			x0_tmp=pixToCmX(width.get(1),y0);
 			x1_tmp=pixToCmX(width.get(3),y1);
 
+			//if any error has been making by the regression, we correct the wall as follows
 			double landmark=pixToCmX(70,(y0+y1)/2);
-
+			//70*4=400 and 600x400 is the resolution of the current camera. Generally an error may occur when the wall spread on 400px
+			//In this case, the regression may not be precious. So, we decided to put a small wall there. Because the robot moves with 70cm and there are 
+			// a huge change to correct the previous wall by recovering it
 			x0_tmp-=landmark*4;
 			x1_tmp-=landmark*4;
 			if(x0_tmp>100.0) {
@@ -335,16 +342,20 @@ public class Exploration {
 			if(x1_tmp>100.0) {
 				x1_tmp=40;
 			}
+
+			//transformation : translation and rotation of the robot to from the robot's system to the main system
 			x0=(float) (x0_tmp*((float)Math.cos(robRotation))-y0_tmp*Math.sin(robRotation)+robPosition.x*100);
 			y0=(float) (y0_tmp*((float)Math.cos(robRotation))+x0_tmp*Math.sin(robRotation)+robPosition.y*100);
 			x1=(float) (x1_tmp*((float)Math.cos(robRotation))-y1_tmp*Math.sin(robRotation)+robPosition.x*100);
 			y1=(float) (y1_tmp*((float)Math.cos(robRotation))+x1_tmp*Math.sin(robRotation)+robPosition.y*100);
 
+			//conversion to 1/100 centimeter
 			x0/=100;
 			y0/=100;
 			x1/=100;
 			y1/=100;
 
+			//adjust the y coordinate to the mean of the y coordinate of the first corner and the second one
 			cornerLeft=new Point(x0,(y1+y0)/2);
 			cornerRight=new Point(x1,(y0+y1)/2);
 			lenSeg=(float) Math.sqrt(Math.pow((x0-x1),2)+Math.pow((y0-y1),2));
@@ -355,19 +366,21 @@ public class Exploration {
 			this.env.adjustCordNearestWalls();
 		}
 
-		//the autonomous robot detect a segment (Horizontal) only
+
 		if(width.get(0)!=-1) { 
 			if(depthL.get(0)==-1 ) {
 				if(depthR.get(0)==-1) {
-					System.out.println("100");
+					//System.out.println("100");
+					//the autonomous robot detects a segment (Horizontal) only
 					if(width.get(2)>threshClose){
-						System.out.println("Close");
+						//the detected segment  is close
+						//System.out.println("Close");
 						try {
 							this.lastDistanceTraveled=0;
 							this.robot.rotate(90);
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
 							if((this.lastDistanceTraveled)<(this.distanceRob/100) ){
-								System.out.println("Distance not totally travelled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
+								System.out.println("Distance not totally traveled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
 								return 0;
 							}
 							this.robot.rotate(-90);
@@ -377,7 +390,8 @@ public class Exploration {
 						}
 					}
 					else {
-						System.out.println("Far");
+						//System.out.println("Far");
+						//the detected segment is too far, the wall is far. The robot may miss some details. So it moves forward
 						try {
 							this.lastDistanceTraveled=0;
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
@@ -388,15 +402,15 @@ public class Exploration {
 					}
 				}
 				else {
-					System.out.println("101");
+					//System.out.println("101");
 					if (width.get(2)>threshClose){
-						System.out.println("Close");
+						//System.out.println("Close");
 						try {
 							this.lastDistanceTraveled=0;
 							this.robot.rotate(90);
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
 							if((this.lastDistanceTraveled)<(this.distanceRob/100) ){
-								System.out.println("Distance not totally travelled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
+								System.out.println("Distance not totally traveled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
 								return 0;
 							}
 							this.robot.rotate(-90);
@@ -407,7 +421,8 @@ public class Exploration {
 						}
 					}
 					else {
-						System.out.println("Far");
+						//System.out.println("Far");
+						//the detected segment is too far, the wall is far. The robot may miss some details. So it moves forward
 						try {
 							this.lastDistanceTraveled=0;
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
@@ -420,15 +435,15 @@ public class Exploration {
 			}
 			else {
 				if(depthR.get(0)==-1) {
-					System.out.println("110");
+					//System.out.println("110");
 					if(width.get(2)>threshClose){
-						System.out.println("Close");
+						//System.out.println("Close");
 						try {
 							this.lastDistanceTraveled=0;
 							this.robot.rotate(90);
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
 							if(this.lastDistanceTraveled<this.distanceRob/100) {
-								System.out.println("Distance not totally travelled");
+								System.out.println("Distance not totally traveled");
 								return 0;
 							}
 							this.robot.rotate(-90);
@@ -438,7 +453,8 @@ public class Exploration {
 						return 0;
 					}
 					else {
-						System.out.println("Far");
+						//System.out.println("Far");
+						//the detected segment is too far, the wall is far. The robot may miss some details. So it moves forward
 						try {
 							this.lastDistanceTraveled=0;
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
@@ -448,15 +464,15 @@ public class Exploration {
 					}
 				}
 				else {
-					System.out.println("111");
+					//System.out.println("111");
 					if (width.get(2)>threshClose){
-						System.out.println("Close");
+						//System.out.println("Close");
 						try {
 							this.lastDistanceTraveled=0;
 							this.robot.rotate(90);
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
 							if((this.lastDistanceTraveled)<(this.distanceRob/100) ){
-								System.out.println("Distance not totally travelled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
+								System.out.println("Distance not totally traveled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
 								return 0;
 							}
 							this.robot.rotate(-90);
@@ -467,7 +483,8 @@ public class Exploration {
 						return 0;
 					}
 					else {
-						System.out.println("Far");
+						//System.out.println("Far");
+						//the detected segment is too far, the wall is far. The robot may miss some details. So it moves forward
 						try {
 							this.lastDistanceTraveled=0;
 							this.lastDistanceTraveled=this.robot.move(distanceRob);
@@ -484,7 +501,7 @@ public class Exploration {
 		else {
 			if(depthL.get(0)==-1 ) {
 				if(depthR.get(0)==-1) {
-					System.out.println("000");
+					//System.out.println("000");
 					try {
 						this.lastDistanceTraveled=0;
 						this.lastDistanceTraveled=this.robot.move(distanceRob);
@@ -495,13 +512,13 @@ public class Exploration {
 
 				}
 				else {
-					System.out.println("001");
+					//System.out.println("001");
 					try {
 						this.lastDistanceTraveled=0;
 						this.robot.rotate(90);
 						this.lastDistanceTraveled=this.robot.move(distanceRob);
 						if((this.lastDistanceTraveled)<(this.distanceRob/100) ){
-							System.out.println("Distance not totally travelled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
+							System.out.println("Distance not totally traveled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
 							return 0;
 						}
 						this.robot.rotate(-90);
@@ -515,12 +532,12 @@ public class Exploration {
 			}
 			else {
 				if(depthR.get(0)==-1) {
-					System.out.println("010");
+					//System.out.println("010");
 					try {
 						this.lastDistanceTraveled=0;
 						this.lastDistanceTraveled=this.robot.move(distanceRob);
 						if((this.lastDistanceTraveled)<(this.distanceRob/100) ){
-							System.out.println("Distance not totally travelled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
+							System.out.println("Distance not totally traveled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
 							return 0;
 						}
 						return 0;
@@ -530,12 +547,12 @@ public class Exploration {
 					return 0;
 				}
 				else {
-					System.out.println("011");
+					//System.out.println("011");
 					try {
 						this.lastDistanceTraveled=0;
 						this.lastDistanceTraveled=this.robot.move(distanceRob);
 						if((this.lastDistanceTraveled)<(this.distanceRob/100) ){
-							System.out.println("Distance not totally travelled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
+							System.out.println("Distance not totally traveled: Last"+(int)this.lastDistanceTraveled+ " robDistanece"+(int)this.distanceRob);
 							return 0;
 						}
 						return 0;
